6b1f8b9742bcb12612f8f8e3b4e51555a704fb83
Avtandil <a.shaburishvili@gmail.com>
2021-09-23 16:33:39 +0400

    footer

--- src/Components/AppFooter/AppFooter.js
+++ src/Components/AppFooter/AppFooter.js
@@ -8,7 +8,7 @@ const AppFooter = () => {
                 <div className='license'>
                     <div className='seal'>
                         <a target='_blank' href="https://validator.antillephone.com/validate?domain=www.mima.games&seal_id=f99f8774c37b3f9360d0a72be4b3148ea3e6cb86ad75728f48326d4b324b3e6000cab5d1ac0331f67c658fba572929c8&stamp=c7a7839cdf65d1fc84f8d1227ac2586c">
-                            <img src='https://1646ba92-8c4a-4d0c-a594-a0ed5da8408d.snippet.antillephone.com/sealassets/c7a7839cdf65d1fc84f8d1227ac2586c-www.mima.games-f99f8774c37b3f9360d0a72be4b3148ea3e6cb86ad75728f48326d4b324b3e6000cab5d1ac0331f67c658fba572929c8-c2VhbC5wbmc%3D?status=valid' />
+                            <img src='../../Assets/images/seal.png' />
                         </a>
 
                     </div>
