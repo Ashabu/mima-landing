003c0b8e0008274a5e02d4bb4c4a80b67a13c1f0
Avtandil Shaburishvili <a.shaburishvili@gmail.com>
2021-10-24 23:34:49 +0400

    pre and after app loaders

---
+++ src/Components/AppLoader/AppLoader.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import './appLoader.scss';
+
+const AppLoader = () => {
+    return (
+        <div className = 'app-loader-main'>
+            <div className = 'app-loader-body'>
+                <div className = 'app-loader-element'>
+
+                </div>
+            </div>
+            
+        </div>
+    );
+};
+
+export default AppLoader;
---
+++ src/Components/AppPreLoader/AppPreLoader.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import './preLoader.scss';
+
+const AppPreLoader = () => {
+    return (
+        <div className = 'pre-loader'>
+            <div className = 'pre-load-content'>
+                <div className = 'pre-load-img'></div>
+            </div>
+            <div className = 'pre-load-loader'>
+                    
+            </div>
+            
+        </div>
+    );
+};
+
+export default AppPreLoader;
